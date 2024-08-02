1ab0a9fe68198dfec6c87ab96f18000d10e2c4ce
AurianLira <aurianmoura972@gmail.com>
2024-08-02 15:57:31 -0300

    1o commit

---
+++ WepApp/src/main/java/br/edu/ifpe/recife/gcms/WepApp/WepAppApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.recife.gcms.WepApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WepAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WepAppApplication.class, args);
+	}
+
+}
