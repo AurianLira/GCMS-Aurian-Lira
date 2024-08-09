c28669e66e2088a0ed94ebf321f22cb6f36254db
AurianLira <aurianmoura972@gmail.com>
2024-08-09 15:11:37 -0300

    Adiciona endpoint e teste/hotfix

--- src/test/java/br/edu/ifpe/recife/gcms/WepApp/WepAppApplicationTests.java
+++ src/test/java/br/edu/ifpe/recife/gcms/WepApp/WepAppApplicationTests.java
@@ -33,5 +33,14 @@ class WebAppApplicationTests {
 				.andExpect(content().string(containsString("Hello, Aurian Moura de Lira!")));
 
 	}
+	
+	@Test
+	public void shouldReturnDefaultMessageHotfix() throws Exception {
+		this.mockMvc.perform(get("/hotfix"))
+
+				.andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello, Hotfix!")));
+
+	}
 
 }
